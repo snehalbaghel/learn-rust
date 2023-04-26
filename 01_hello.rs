4758c2ac2de9e9797bc29f8be0dd05ebf2d8f55c
Snehal Baghel <snehalbaghel@gmail.com>
2023-04-26 20:28:40 +0530

    initial commit

---
+++ 01_hello.rs
@@ -0,0 +1,5 @@
+fn main() {
+	println!("Hello World!");
+	println!("{number:0>width$}", number=1, width=5);
+
+}
---
+++ 03_impl.rs
@@ -0,0 +1,20 @@
+use std::fmt;
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}i", self.real, self.imag)
+    }
+}
+
+fn main() {
+    let comp = Complex { real: 3.3, imag: 7.2 };
+
+    println!("Display: {}", comp);
+    println!("Debug: {:?}", comp);
+}
